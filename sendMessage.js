a8153907a94010b5910663bb7afe91b5c3930594
melchi55 <melchi.melchi6@gmail.com>
2021-07-13 11:23:35 +0530

    added new files

--- sendMessage.js
+++ sendMessage.js
@@ -1,19 +1,11 @@
 const axios = require("axios");
 
-const slackToken = "xoxb-2248581341719-2263554854451-b48SuTFLxu0Au9ZHL3K2ZWa6";
+const slackToken = "xoxb-2248581341719-2263554854451-6e1twyGdOn7C7aXpSYpxcfld";
 
 run().catch((err) => console.log(err));
 
 async function run() {
   const url = "https://slack.com/api/chat.postMessage";
-  //   const res = await axios.post(
-  //     url,
-  //     {
-  //       channel: "#test",
-  //       text: "Hello, World!",
-  //     },
-  //     { headers: { authorization: `Bearer ${slackToken}` } }
-  //   );
   const res = await axios.post(
     url,
     {
